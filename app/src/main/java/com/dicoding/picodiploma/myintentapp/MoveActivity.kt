16523aa5191f9838d2c3ed71b5fa7f7eb5927880
eka-ant16 <muktieka44@gmail.com>
2020-09-30 13:38:13 +0700

    first commit

---
+++ app/src/main/java/com/dicoding/picodiploma/myintentapp/MoveActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.picodiploma.myintentapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MoveActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move)
+    }
+}
---
+++ app/src/main/java/com/dicoding/picodiploma/myintentapp/MoveWithDataActivity.kt
@@ -0,0 +1,28 @@
+package com.dicoding.picodiploma.myintentapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class MoveWithDataActivity : AppCompatActivity() {
+
+    companion object{
+        const val EXTRA_AGE = "extra_age"
+        const val EXTRA_NAME = "extra_name"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move_with_data)
+
+        val tvDataReceived: TextView = findViewById(R.id.tv_data_received)
+
+        val name = intent.getStringExtra(EXTRA_NAME)
+        val age = intent.getIntExtra(EXTRA_AGE, 0)
+
+        val text = "Name : $name, Your Age : $age"
+        tvDataReceived.text = text
+
+    }
+}
